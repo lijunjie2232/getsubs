0e1072852f004f8e1b5f69c420d7c3ceb30b832b
lijunjie2232 <2533584225@qq.com>
2023-01-26 17:12:58 +0800

    update

--- Free-servers.py
+++ Free-servers.py
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 
         # form sub
         sub1 = str(encode(nodeList))
-        with open("./Free-servers.txt", "w") as f:
+        with open("./Free-servers", "w") as f:
             f.write(sub1)
             f.close()
 
