fc4c9a72e1503d586ba34d25c78157576e436d73
lijunjie2232 <2533584225@qq.com>
2023-02-24 14:56:08 +0800

    update something

--- Free-servers.py
+++ Free-servers.py
@@ -5,10 +5,12 @@ import requests
 import re
 import base64
 
-def decode(sub:str):
+
+def decode(sub: str):
     return base64.b64decode(sub).decode('utf-8').split('\n')
 
-def encode(subs:list):
+
+def encode(subs: list):
     str = ""
     for i in subs:
         if str:
@@ -17,31 +19,96 @@ def encode(subs:list):
     return base64.b64encode(str.encode()).decode('utf-8')
 
 
+def html_filter(url: str, rule: str = None):
+    resp = requests.get(url)
+    resp.encoding = "utf-8"
+    if rule == None:
+        try:
+            return resp.text
+        except Exception as e:
+            print(e)
+    try:
+        text = requests.get(url).text
+        # print(text)
+        pattern = re.compile(rule)
+        result = pattern.findall(text)
+        return result
+    except Exception as e:
+        print(e)
+
+
+def text_filter(text: str, rule: str):
+    try:
+        pattern = re.compile(rule)
+        result = pattern.findall(text)
+        return result
+    except Exception as e:
+        print(e)
+
+
+vsub_data = [
+    [
+        ("https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md",
+         r"v2ray订阅链接[\s\S]*?(http.*?)\n"),
+        (None)
+    ],
+
+    [
+        ("https://raw.githubusercontent.com/mksshare/mksshare.github.io/main/README.md",
+         r"```[\s]*?([\s\S]*)[\s]*?```"),
+        (r"\n(.*?)\n")
+    ],
+
+]
+
 if __name__ == '__main__':
     try:
+        ### v2ray sub
         nodeList = set()
 
         # get sub url
-        resp = requests.get("https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md")
-        text = resp.text
-        pattern = re.compile(r"v2ray订阅链接[\s\S]*?(http.*?)\n")
-        result = pattern.findall(text)
+        result = html_filter(
+            "https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md", r"v2ray订阅链接[\s\S]*?(http.*?)\n")
 
         # get sub
-        resp = requests.get(result[0])
-        sub = resp.text
+        sub = html_filter(result[0])
 
         # node aggr
         nodes = set(decode(sub))
         nodes.remove('')
         nodeList = nodeList.union(nodes)
-        len(nodeList)
 
-        # form sub
-        sub1 = str(encode(nodeList))
-        with open("./Free-servers", "w") as f:
-            f.write(sub1)
-            f.close()
+        result = html_filter(
+            "https://raw.githubusercontent.com/mksshare/mksshare.github.io/main/README.md", r"```[\s]*?([\s\S]*)[\s]*?```")[0]
+        result = text_filter(result, r"\n(.*?)\n")
+        nodes = set(result)
+        nodes.remove('')
+        nodeList = nodeList.union(nodes)
+
+        # form sub and write into file
+        nodes_sub = str(encode(nodeList))
+        files_v = [
+            "./Free-servers",
+            "./Free-servers.txt"
+        ]
+        for f in files_v:
+            with open(f, "w") as f:
+                f.write(nodes_sub)
+
+
+        ### clash sub
+        clash_url = html_filter(
+            "https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md", r"clash订阅链接[\s\S]*?(http.*?)\n")
+        # print(clash_url)
+        clash = html_filter(clash_url[0])
+
+        # write into file
+        files_c = [
+            "clash.yaml"
+        ]
+        for f in files_c:
+            with open(f, "w") as f:
+                f.write(clash)
 
     except Exception as e:
         print(e)
