279b1dc41c876ed858ab2aa51150f84a2223b4d3
lijunjie2232 <2533584225@qq.com>
2023-07-10 11:09:31 +0800

    hard learning

--- Free-servers.py
+++ Free-servers.py
@@ -48,44 +48,55 @@ def text_filter(text: str, rule: str):
 
 vsub_data = [
     [
-        ("https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md",
-         r"v2ray订阅链接[\s\S]*?(http.*?)\n"),
-        (None)
+        "https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md",
+        ('h', r"v2ray订阅链接[\s\S]*?(http.*?)\n", 0),
+        ('h', None, -1),
+        1
     ],
-
     [
-        ("https://raw.githubusercontent.com/mksshare/mksshare.github.io/main/README.md",
-         r"```[\s]*?([\s\S]*)[\s]*?```"),
-        (r"\n(.*?)\n")
+        "https://raw.githubusercontent.com/mksshare/mksshare.github.io/main/README.md",
+        ('h', r"```[\s]*?([\s\S]*)[\s]*?```" , 0),
+        ('t', r"\n(.*?)\n", -1),
+        0
     ],
 
+]
+csub_data = [
+    
 ]
 
 if __name__ == '__main__':
     try:
         ### v2ray sub
         nodeList = set()
-
-        # get sub url
-        result = html_filter(
-            "https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md", r"v2ray订阅链接[\s\S]*?(http.*?)\n")
-
-        # get sub
-        sub = html_filter(result[0])
-
-        # node aggr
-        nodes = set(decode(sub))
-        nodes.remove('')
-        nodeList = nodeList.union(nodes)
-
-        # get sub
-        result = html_filter(
-            "https://raw.githubusercontent.com/mksshare/mksshare.github.io/main/README.md", r"```[\s]*?([\s\S]*)[\s]*?```")[0]
-        result = text_filter(result, r"\n(.*?)\n")
-        nodes = set(result)
-        nodes.remove('')
-        nodeList = nodeList.union(nodes)
-        print(len(nodeList))
+        
+        result = None
+        for data in vsub_data:
+            result = data[0]
+            for action in data[1:-1]:
+                if action[0] == 'h':
+                    if action[-1] == -1:
+                        result = html_filter(result, action[1])
+                    else:
+                        result = html_filter(result, action[1])[action[-1]]
+                elif action[0] == "t":
+                    if action[-1] == -1:
+                        result = text_filter(result, action[1])
+                    else:
+                        result = text_filter(result, action[1])[action[-1]]
+                # print(result)
+            if result:
+                if data[-1]:
+                    nodes = set(decode(result))
+                else:
+                    nodes = set(result)
+            
+                if '' in nodes:
+                    nodes.remove('')
+                nodeList = nodeList.union(nodes)
+                # print(data[0], '\nnums: ' , len(nodes))
+            else:
+                pass
 
         # form sub and write into file
         nodes_sub = str(encode(nodeList))
@@ -94,23 +105,23 @@ if __name__ == '__main__':
             "./Free-servers.txt"
         ]
         for f in files_v:
-            with open(f, "w") as f:
+            with open(f, "w", encoding='utf-8') as f:
                 f.write(nodes_sub)
-
-
+            
         ### clash sub
         clash_url = html_filter(
             "https://raw.githubusercontent.com/FiFier/v2rayShare/main/README.md", r"clash订阅链接[\s\S]*?(http.*?)\n")
         # print(clash_url)
         clash = html_filter(clash_url[0])
+        # print(clash)
 
         # write into file
         files_c = [
             "clash.yaml"
         ]
         for f in files_c:
-            with open(f, "w") as f:
+            with open(f, "w", encoding='utf-8') as f:
                 f.write(clash)
 
     except Exception as e:
-        print(e)
+        print(repr(e))
